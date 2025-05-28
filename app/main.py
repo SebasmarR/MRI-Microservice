42a2601449eaf0a516cb3bc9910c00e7c21666b5
SebasmarR <sebastianmartinezarias@gmail.com>
2025-05-28 02:42:24 -0500

    pymong

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.include_router(auth_router) 
-app.include_router(mri.router, prefix="/mris", tags=["MRIs"]) 
+app.include_router(mri.router, prefix="/mri", tags=["MRIs"]) 
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
