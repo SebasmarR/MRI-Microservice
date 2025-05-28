5a7730c4974c99a16982d62c362faa451e922d70
SebasmarR <sebastianmartinezarias@gmail.com>
2025-05-28 03:14:08 -0500

    static

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.include_router(auth_router) 
 app.include_router(mri.router, prefix="/mri", tags=["MRIs"]) 
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "app", "static")), name="static")
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
