23ba7fe8f67f4171f6a7cf7d57f9c5fade0eb437
SebasmarR <sebastianmartinezarias@gmail.com>
2025-05-28 03:06:55 -0500

    imagenes2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from dotenv import load_dotenv
@@ -19,6 +20,7 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.include_router(auth_router) 
 app.include_router(mri.router, prefix="/mri", tags=["MRIs"]) 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
